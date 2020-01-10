16e41e7edddcdc634ec3220eda67140289c351b4
Mahesh Rapolu <mr19394@corp.ojas-it.com>
2020-01-10 18:00:08 +0530

    fileupload

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, ViewChild } from '@angular/core';
+import { CSVRecord } from './CSVModel';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'Angular7-readCSV';
+  status: boolean = true;
+  status1: boolean = false;
+  data: any;
+  searchText:any
+  sample:any;
+  fn(e) {
+    this.data = e;
+    this.status = false;
+    this.status1 = true;
+  }
+  
+  key(sample){
+    this.sample = this.searchText;
+    }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { DataComponent } from './data/data.component';
+import { DetailsComponent } from './details/details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DataComponent,
+    DetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
